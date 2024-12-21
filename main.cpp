5c5056178cc71a3e1533624d56aa46486e560f68
Nikita <zazoozazoozaza@gmail.com>
2024-12-21 14:17:40 +0300

    Added Contains tests and Candle is valid function

--- main.cpp
+++ main.cpp
@@ -7,35 +7,62 @@
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
-//тест 1
+//Check valid candle
+bool CandleExist(Candle& candle)
+{
+  return (candle.open < candle.high &&
+   candle.low < candle.close &&
+   candle.low < candle.high);
+}
+
 bool BodyContains_ByDefaultPrice_ReturnsTrue()
 {
   Candle candle(10, 25, 5, 20); //GreenCandle
   
-  return (candle.body_contains(20) == true);
+  return ((candle.body_contains(20) && CandleExist(candle)) == true);
 }
-
 bool BodyContains_PriceOverParameters_ReturnsFalse()
 {
   Candle candle(20, 25, 5, 10); //RedCandle
-  
-  return (candle.body_contains(30) == false);
-}
 
+  return ((candle.body_contains(30) && CandleExist(candle)) == false);
+}
 bool BodyContains_CandleDoesNotExist_ReturnsFalse()
 {
   Candle candle(20, 10, 10, 30); //DoesNotExist _open > _high, _low < _close
-  
-  return (candle.body_contains(0) == false);
+
+  return ((candle.body_contains(0) && CandleExist(candle)) == false);
 }
-//
+
+bool Contains_ByDefaultPrice_ReturnsTrue()
+{
+  Candle candle(10, 25, 5, 20);
+  
+  return ((candle.contains(20) && CandleExist(candle)) == true);
+}
+bool Contains_PriceOverParameters_ReturnsFalse()
+{
+  Candle candle(20, 25, 5, 10);
+  
+  return ((candle.contains(30) && CandleExist(candle)) == false);
+}
+bool Contains_ZeroCandle_ReturnsFalse()
+{
+  Candle candle(0, 0, 0, 0);
+
+  return ((candle.contains(0) && CandleExist(candle)) == false);
+}
+
 
 void initTests()
 {
   tests.push_back(BodyContains_ByDefaultPrice_ReturnsTrue);
   tests.push_back(BodyContains_PriceOverParameters_ReturnsFalse);
   tests.push_back(BodyContains_CandleDoesNotExist_ReturnsFalse);
-  
+
+  tests.push_back(Contains_ByDefaultPrice_ReturnsTrue);
+  tests.push_back(Contains_PriceOverParameters_ReturnsFalse);
+  tests.push_back(Contains_ZeroCandle_ReturnsFalse);
 }
 
 int launchTests()
